e98ebfc2a0a56076b1ce08af45c95eef47c98aea
John Pachkoski <jpachkoski@gmail.com>
2024-03-23 00:15:22 -0400

    Moved properties

--- Chapter05/Travel/src/core/Travel.Domain/Entities/TourList.cs
+++ Chapter05/Travel/src/core/Travel.Domain/Entities/TourList.cs
@@ -6,13 +6,12 @@ namespace Travel.Domain.Entities
   {
     public TourList()
     {
-      Tours = new List<TourPackage>();
+        Tours = new List<TourPackage>();
     }
-
-    public IList<TourPackage> Tours { get; set; }
     public int Id { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
     public string About { get; set; }
+    public IList<TourPackage> Tours { get; set; }
   }
 }
